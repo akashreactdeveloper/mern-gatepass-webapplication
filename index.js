200a9705a7b1ed3e646ffd44e161a3c7a56db6d1
Your Name <your-email@example.com>
2024-07-27 11:22:37 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+const connectDB = require('./config/db')
+const router = require('./routes')
+const cookieParser = require('cookie-parser')
+const AwlModel = require('./models/AwlModel');
+
+const app = express()
+app.use(cors());
+
+app.use(express.json())
+app.use(cookieParser())
+
+app.use("/api", router)
+
+const PORT = 8080 || process.env.PORT
+
+
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log("connect to DB")
+        console.log("Server is running" + PORT)
+    })
+})
